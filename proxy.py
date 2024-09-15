b75fbd89e700559aca177b829b692e14e6857b9f
felipe-andrade <87283195+Astratta@users.noreply.github.com>
2024-09-15 23:38:00 +0000

    Updated

--- proxy.py
+++ proxy.py
@@ -1,5 +1,48 @@
 import httpx
-from httpx import BasicAuth, Limits, Timeout
+import json
+from typing import Dict, Callable
+from httpx import Limits, Timeout
+
+def _format_request(request):
+  def wrapper(**kwargs):
+    if "data" in kwargs:
+      kwargs["data"] = json.dumps(kwargs["data"])
+    return request(**kwargs)
+  return wrapper
+
+def _pagination_handler(request: Callable):
+    def wrapper(endpoint, method, pagination, **kwargs):
+        def _offset_limit_based():
+            pages = []
+            offset = kwargs["params"][pagination["fields"][0]]
+            limit = kwargs["params"][pagination["fields"][1]]
+
+            while True:
+                exhausted = False
+                r = request(endpoint, method, pagination, **kwargs)
+                
+                for key in r.keys():
+                    if key == "total":
+                        continue
+
+                    if len(r[key]) == 0:
+                        exhausted = True
+                        break
+                    
+                    pages.append(r[key])
+
+                if exhausted:
+                    break
+                    
+                offset += limit
+                kwargs["params"][pagination["fields"][0]] = offset ## Offset
+                #kwargs["params"][pagination["fields"][1]] ## Limit
+            return pages
+
+        if pagination["type"] == "offset_limit":
+            return _offset_limit_based()
+
+    return wrapper
 
 class Proxy:
     def __init__(self):
@@ -14,9 +57,15 @@ class Proxy:
             limits=Limits(max_connections=100, max_keepalive_connections=20),
             transport=httpx.HTTPTransport(retries=retries)
         )
-    
-    def _build_request():
-        ...
 
-    def _pagination_handler():
-        ...
+    @_format_request
+    @_pagination_handler
+    async def make_request(self, endpoint: str, method: str, pagination: Dict, **kwargs):
+        try:
+            r = self.client.request(method, endpoint, **kwargs)
+            r.raise_for_status()
+            return r.json()
+        except httpx.HTTPStatusError as e:
+            print(f"Request failed: {e}")
+        except httpx.RequestError as e:
+            print(f"An error occurred: {e}")
